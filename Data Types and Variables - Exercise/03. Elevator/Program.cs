ae929bbe20dd568664e82bdb33805c32ee5a4416
Boyan Boychev <boyanboychev@gmail.com>
2022-09-25 11:47:30 +0100

    Adding task "03. Elevator"
    Signed-off-by: Boyan Boychev <boyanboychev@gmail.com>

---
+++ Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculate how many courses will be needed to elevate n persons by using an elevator of the capacity of p persons. The input
+            //holds two lines: the number of people n and the capacity p of the elevator.
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+
+            double numberOfCourses = (double)numberOfPeople / elevatorCapacity;
+            double roundedNumberOfCourses = Math.Ceiling(numberOfCourses);
+
+            Console.WriteLine(roundedNumberOfCourses);
+        }
+    }
+}
