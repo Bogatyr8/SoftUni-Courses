2a7e0b6abb90f7110cc017972cc8d3bbabba28d3
Boyan Boychev <boyanboychev@gmail.com>
2022-09-25 11:26:30 +0100

    Adding task "02. Sum Digits"

---
+++ Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            int inputLength = input.Length;
+            int sum = 0;
+            for (int i = 1; i <= inputLength; i++)
+            {
+                int lastDigit = number % 10;
+                sum += lastDigit;
+                number /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
