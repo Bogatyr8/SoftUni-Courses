c7eb670e5ae64f7c26979e85894a7a98953fcd85
Boyan Boychev <boyanboychev@gmail.com>
2022-09-29 00:01:06 +0100

    Including "Data Types and Variables - More Exercise.sln" with first task "01. Data Type Finder"

--- Data Types and Variables - Exercise/11. Snowballs - 2/Program.cs
+++ Data Types and Variables - Exercise/11. Snowballs - 2/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Numerics;
 
-namespace _11._Snowballs___2
+namespace _11._Snowballs
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Tony and Andi love playing in the snow and having snowball fights, but they always argue about which makes the best snowballs.
+            //They have decided to involve you in their fray, by making you write a program, which calculates snowball data and outputs the best
+            //snowball value.
+            //You will receive N – an integer, the number of snowballs being made by Tony and Andi.
+            //For each snowball you will receive 3 input lines:
+            //•	On the first line, you will get the snowballSnow – an integer.
+            //•	On the second line, you will get the snowballTime – an integer.
+            //•	On the third line, you will get the snowballQuality – an integer.
+            //For each snowball you must calculate its snowballValue by the following formula:
+            //(snowballSnow / snowballTime) ^ snowballQuality
+            //In the end, you must print the highest calculated snowballValue.
+            //Input
+            //•	On the first input line, you will receive N – the number of snowballs.
+            //•	On the next N *3 input lines you will be receiving data about snowballs.
+            //Output
+            //•	As output, you must print the highest calculated snowballValue, by the formula, specified above.
+            //•	The output format is: 
+            //{ snowballSnow} : { snowballTime} = { snowballValue} ({ snowballQuality})
+            //Constraints
+            //•	The number of snowballs(N) will be an integer in the range[0, 100].
+            //•	The snowballSnow is an integer in the range[0, 1000].
+            //•	The snowballTime is an integer in the range[1, 500].
+            //•	The snowballQuality is an integer in the range[0, 100].
+            int n = int.Parse(Console.ReadLine());
+            BigInteger maxSnowballValue = BigInteger.MinusOne;
+            int maxSnowballSnow = 0;
+            int maxSnowballTime = 0;
+            int maxSnowballQuality = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+                if (snowballValue > maxSnowballValue)
+                {
+                    maxSnowballValue = snowballValue;
+                    maxSnowballSnow = snowballSnow;
+                    maxSnowballTime = snowballTime;
+                    maxSnowballQuality = snowballQuality;
+                }
+
+            }
+            Console.WriteLine($"{maxSnowballSnow} : {maxSnowballTime} = {maxSnowballValue} ({maxSnowballQuality})");
         }
     }
 }
---
+++ Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive input until you receive "END".Find what data type is the input. Possible data types are:
+            //•	Integer
+            //•	Floating point 
+            //•	Characters
+            //•	Boolean
+            //•	Strings
+            //Print the result in the following format: "{input} is {data type} type"
+
+            int decimalPointCounter = 0;
+            int minusSignCounter = 0;
+            string input = Console.ReadLine();
+            while (input != "END" && input != "end")
+            {
+                bool isItInteger = false;
+                bool isItFloat = false;
+                bool isItCharacter = false;
+                bool isItBoolean = false;
+                bool isItString = false;
+                int inputLength = input.Length;
+
+
+                if (input[0] >= '0' && input[0] <= '9' || input[0] == '-' || 
+                    input[0] == '.' || input[0] == ',')
+                {
+                    for (int i = 0; i <= inputLength - 1; i++)
+                    {
+                        char index = input[i];
+                        bool isItNumber = index >= '0' && index <= '9' || index == '-' || 
+                            index == '.' || index == ',' || index == '/' || index == '+' || index == '*';
+                        if (isItNumber && index == '-')
+                        {
+                            minusSignCounter++;
+                            if (i != 0) // this prevents acknoledging a number type with minus sign in the middle
+                            {
+                                isItInteger = false;
+                                isItFloat = false;
+                                break;
+                            }
+                        }
+                        if (isItNumber && (index == '.' || index == ',' || index == '/'))
+                        {
+                            decimalPointCounter++;
+                        }
+                        if (isItNumber && decimalPointCounter <= 1 && minusSignCounter <= 1)
+                        {
+                            if (index == '.' || index == ',' || index == '/')
+                            {
+                                isItFloat = true;
+                                isItInteger = false;
+                                if (i == inputLength - 1) // this prevents acknoledging floating number type for no digits, after the poin/comma
+                                {
+                                    isItInteger = false;
+                                    isItFloat = false;
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                if (!isItFloat)
+                                {
+                                    isItInteger = true;
+                                }
+                            }
+                        }
+                        else if (!(isItNumber && decimalPointCounter <= 1 && minusSignCounter <= 1)|| !isItNumber)
+                        {
+                            isItInteger = false;
+                            isItFloat = false;
+                            break; // it is NOT a number. Continue with identification of the data type...
+                        }
+                    }
+                }
+                if (input == "true" || input == "TRUE" || input == "True" || input == "TRue" || 
+                    input == "TRUe" || input == "tRUE" || input == "trUE" || input == "truE" || 
+                    input == "TRuE" || input == "TrUE" || input == "false" || input == "FALSE" || 
+                    input == "False" || input == "FALSe" || input == "FALse" || input == "FAlse" || 
+                    input == "falsE" || input == "falSE" || input == "faLSE" || input == "fALSE" || 
+                    input == "FalsE" || input == "FAlSE" || input == "fAlSe" || input == "FaLsE")
+                {
+                    isItBoolean = true;
+                }
+                if (inputLength == 1 && !(isItFloat || isItInteger))
+                {
+                    isItCharacter = true;
+                }
+                if (inputLength != 1 && !(isItFloat || isItInteger) && input != "END" && input != "end")
+                {
+                    isItString = true;
+                }
+
+
+                if (isItInteger)
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (isItFloat)
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (isItBoolean)
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else if (isItCharacter)
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (isItString)
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                decimalPointCounter = 0;
+                minusSignCounter = 0;
+                input = Console.ReadLine();
+            }
+         }
+    }
+}
