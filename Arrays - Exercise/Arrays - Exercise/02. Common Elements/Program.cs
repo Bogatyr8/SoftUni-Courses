d798e331a91112551d2f3fd8e1251995174d643a
Boyan Boychev <boyanboychev@gmail.com>
2022-10-03 23:30:30 +0100

    Adding task "02. Common Elements "

---
+++ Arrays - Exercise/Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Create a program that prints out all common elements in two arrays. You have to compare the elements of
+//the second array to the elements of the first.
+            string[] str1 = Console.ReadLine().Split();
+            string[] str2 = Console.ReadLine().Split();
+            string[] temporary = new string[str1.Length];
+            int counter = 0;
+            for (int i = 0; i < str2.Length; i++)
+            {
+                for (int j = 0; j < str1.Length; j++)
+                {
+                    if (str2[i] == str1[j])
+                    {
+                        temporary[counter] += str2[i];
+                        counter++;
+                        break;
+                    }
+                }
+            }
+            string[] common = new string[counter];
+            for (int i = 0; i < counter; i++)
+            {
+                common[i] = temporary[i];
+            }
+            Console.WriteLine(string.Join(" ", common));
+        }
+    }
+}
