6e3a9cf6b2f7d7be9d36676642c36560351a0331
Boyan Boychev <boyanboychev@gmail.com>
2022-10-03 20:59:03 +0100

    Including "Arrays - Exercise" with first task "01. Train"

---
+++ Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A train has an **n * *number of wagons(integer, received as input). On the next n lines, you will receive
+            //the number of people that are going to get on each wagon.Print out the number of passengers in each wagon
+            //followed by the total number of passengers on the train.
+            int n = int.Parse(Console.ReadLine());
+            int[] wagons = new int[n];
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+                sum += wagons[i];
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+            Console.WriteLine(sum);
+        }
+    }
+}
