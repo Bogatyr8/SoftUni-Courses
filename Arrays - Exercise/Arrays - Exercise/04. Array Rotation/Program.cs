7455b929c5b89ac17a8cf002eaf7d89b6d8f0f1b
Boyan Boychev <boyanboychev@gmail.com>
2022-10-08 02:19:20 +0100

    Added task "04. Array Rotation " (not complete)

---
+++ Arrays - Exercise/Arrays - Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that receives an array and several rotations that you have to perform.The rotations are
+            //done by moving the first element of the array from the front to the back. Print the resulting array.
+            //Examples
+            //Input                     Output
+            //51 47 32 61 21            32 61 21 51 47             
+            //2   
+            //32 21 61 1                32 21 61 1              
+            //4   
+            //2 4 15 31                 4 15 31 2                   
+            //5   
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int n = int.Parse(Console.ReadLine());
+            int[] output = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = input[Math.Abs(input.Length - i - (n % input.Length)) % input.Length];
+                //output[i] = input[(2 * input.Length - i - (n % input.Length)) % input.Length];
+                //if (i > n % input.Length)
+                //{
+                //    output[i] = input[Math.Abs((input.Length - i) - (n % input.Length))];
+                //}
+                //else
+                //{
+                //    output[i] = input[(input.Length + i) - (n % input.Length) - 1];
+                //}
+            }
+            Console.WriteLine(string.Join(" ",output));
+        }
+    }
+}
