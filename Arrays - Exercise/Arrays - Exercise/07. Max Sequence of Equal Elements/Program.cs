5ed3848a1581836d86ed855907670dd84ccc832d
Boyan Boychev <boyanboychev@gmail.com>
2022-10-09 10:12:51 +0100

    Added task "07. Max Sequence of Equal Elements"

---
+++ Arrays - Exercise/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that finds the longest sequence of equal elements in an array of integers. If several
+            //equal sequences are present in the array, print out the leftmost one.
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sameDigitCounter = 1;
+            int currentDigit = 0;
+            int maxSameDigitCounter = 0;
+            int maxDigit = 0;
+            for (int i = input.Length - 1; i >= 1; i--)
+            {
+                if ((input[i] - input[i - 1]) == 0)
+                {
+                    currentDigit = input[i];
+                    sameDigitCounter++;
+                }
+                else
+                {
+                    sameDigitCounter = 1;
+                }
+                if (sameDigitCounter >= maxSameDigitCounter)
+                {
+                    maxSameDigitCounter = sameDigitCounter;
+                    maxDigit = currentDigit;
+                }
+            }
+            for (int i = 0; i < maxSameDigitCounter; i++)
+            {
+                Console.Write($"{maxDigit} ");
+            }
+        }
+    }
+}
