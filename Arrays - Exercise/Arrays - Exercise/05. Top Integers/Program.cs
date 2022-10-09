4721c7a04740a0413d26fa8d4db8308a501e0e2b
Boyan Boychev <boyanboychev@gmail.com>
2022-10-09 01:08:01 +0100

    Added task "05. Top Integers"

---
+++ Arrays - Exercise/Arrays - Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine()
+                .Split()
+                .Select(long.Parse)
+                .ToArray();
+            bool isItBiggerThanTheRest = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] < input[j])
+                    {
+                        isItBiggerThanTheRest = false;
+                        break;
+                    }
+                }
+
+                if (!isItBiggerThanTheRest)
+                {
+                    isItBiggerThanTheRest = true;
+                    continue;
+                }
+
+                Console.Write($"{input[i]} ");
+            }
+        }
+    }
+}
