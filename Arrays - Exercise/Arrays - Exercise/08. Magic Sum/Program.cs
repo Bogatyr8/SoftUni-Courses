fe1d1ae65bae73bcc9b9f192f7888d7ec92c39b8
Boyan Boychev <boyanboychev@gmail.com>
2022-10-09 10:42:17 +0100

    Added task "08. Magic Sum"

---
+++ Arrays - Exercise/Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program, which prints all unique pairs in an array of integers whose sum is equal to a
+            //given number.
+            int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+            int entryValue = int.Parse(Console.ReadLine());
+            int[] dualValue = new int[2];
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i; j < input.Length; j++)
+                {
+                    if ((input[i] + input[j]) == entryValue)
+                    {
+                        dualValue[0] = input[i];
+                        dualValue[1] = input[j];
+                        Console.WriteLine(string.Join(" ", dualValue));
+                    }
+                }
+            }
+        }
+    }
+}
