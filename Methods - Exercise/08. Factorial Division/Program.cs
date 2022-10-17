031c9ffb785e8b86db8f60025c3544510e161921
Boyan Boychev <boyanboychev@gmail.com>
2022-10-17 17:42:21 +0100

    Added task "08. Factorial Division"

---
+++ Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read two integers. Calculate the factorial of each number. Divide the first result by the second and print the result of the
+            //division formatted to the second decimal point.
+            BigInteger a = BigInteger.Parse(Console.ReadLine());
+            BigInteger b = BigInteger.Parse(Console.ReadLine());
+
+            double result = (double)Division(Factoriel(a), Factoriel(b));
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static BigInteger Factoriel(BigInteger factoriel)
+        {
+            factoriel = BigInteger.Abs(factoriel);
+            if (factoriel > 0)
+            {
+                for (BigInteger i = factoriel - 1; i >= 2; i--)
+                {
+                    factoriel *= i;
+                }
+            }
+            else
+            {
+                factoriel = 1;
+            }
+            return factoriel;
+        }
+
+        static BigInteger Division(BigInteger a, BigInteger b)
+        {
+            return a/b;
+        }
+    }
+}
