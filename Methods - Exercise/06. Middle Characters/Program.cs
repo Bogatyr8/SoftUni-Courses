ed9485b3ba4e38d0a723c38286706f56d50b4e80
Boyan Boychev <boyanboychev@gmail.com>
2022-10-17 16:47:21 +0100

    Added task "06. Middle Characters"

---
+++ Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive a single string.Create a method that prints the character found at its middle. If the length of the string
+            //is even there are two middle characters.
+            string input = Console.ReadLine();
+            Console.WriteLine(MiddleChars(input));
+        }
+        static string MiddleChars(string text)
+        {
+            string middleText = string.Empty;
+            int middle = 0;
+            if (text.Length % 2 == 0)
+            {
+                middle = (text.Length - 1) / 2;
+                middleText += text[middle];
+                middleText += text[middle + 1];
+            }
+            else
+            {
+                middle = (text.Length - 1) / 2;
+                middleText += text[middle];
+            }
+            return middleText;
+        }
+    }
+}
