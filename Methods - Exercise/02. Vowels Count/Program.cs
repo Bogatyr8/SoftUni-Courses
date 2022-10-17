2f23e7549c680a82edec98fdcf2ee43028d1eb7f
Boyan Boychev <boyanboychev@gmail.com>
2022-10-17 12:22:51 +0100

    Added task "03. Characters in Range "

---
+++ Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a method that receives a single string and prints out the number of vowels contained in it.
+            string input = Console.ReadLine();
+            int vowelcount = 0;
+            vowelcount = VowelCount(input, vowelcount);
+        }
+
+        private static int VowelCount(string input, int vowelcount)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                    case 'Y':
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                    case 'y':
+                        vowelcount++;
+                        break;
+                }
+            }
+
+            Console.WriteLine(vowelcount);
+
+            return vowelcount;
+        }
+    }
+}
---
+++ Methods - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a method that receives two characters and prints all the characters between them according to
+            //ASCII(on a single line).
+            //NOTE: If the second letter's ASCII value is less than that of the first one then the two initial letters
+            //should be swapped.
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char buffer = char.MinValue;
+
+            if (a > b)
+            {
+                buffer = a;
+                a = b;
+                b = buffer;
+            }
+
+            PrintChars(a, b);
+        }
+
+        private static void PrintChars(char a, char b)
+        {
+            for (int i = a + 1; i < b; i++)
+            {
+                char c = (char)i;
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
