9907a4f30edd5f5269fff6fe6f76f1210352024d
Boyan Boychev <boyanboychev@gmail.com>
2022-10-17 16:55:17 +0100

    Added task "07. NxN Matrix"

---
+++ Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Create a method that receives a single integer n and prints an NxN matrix using this number as a filler.
+            int n = int.Parse(Console.ReadLine());
+
+            NxNMatrix(n);
+
+        }
+
+        static void NxNMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
