457bea8515b531f39bddcf211472564f5abe1a1e
Boyan Boychev <boyanboychev@gmail.com>
2022-10-24 22:02:07 +0100

    Added task "01. Messaging" from "Lists - More Exercise"

---
+++ Lists - More Exercise/01. Messaging/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+            List<string> listOfNumbers = numbers
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string hiddenCode = Console.ReadLine();
+
+            List<char> theCode= new List<char>();
+
+            for (int i = 0; i < hiddenCode.Length; i++)
+            {
+                theCode.Add(hiddenCode[i]);
+            }
+
+            int count = 0;
+            string output = string.Empty;
+            while (listOfNumbers.Count != 0)
+            {
+                int sum = 0;
+                int value = int.Parse(listOfNumbers[0]);
+
+                for (int i = 0; i < listOfNumbers[0].Length; i++)
+                {
+                    sum += value % 10;
+                    value /= 10;
+                }
+
+                output += theCode[sum % theCode.Count];
+                theCode.RemoveAt(sum % theCode.Count);
+                listOfNumbers.RemoveAt(0);
+                count++;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
