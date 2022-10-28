efd7def3d194c2155e6d72edaeeabdc10c0780e1
Boyan Boychev <boyanboychev@gmail.com>
2022-10-28 15:01:59 +0100

    Added task "02. Big Factorial" from Objects and Classes - Lab

---
+++ Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive a number N in the range [0…1000]. Calculate the Factorial of N and print out the result.
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
