af6465f8d92d310276faa5c2cda3805dc86eb5ba
Boyan Boychev <boyanboychev@gmail.com>
2022-10-29 11:24:10 +0100

    Added task "05. Students 2.0" from Objects and Classes - Lab (aided by example)

---
+++ Objects and Classes - Lab/05. Students 2.0/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Students_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use the class from the previous problem. If you receive a student, which already exists (first name and last name should be unique) overwrite the
+            //information.
+
+            List<Student> students = new List<Student>();
+
+
+            string inputString;
+            while ((inputString = Console.ReadLine()) != "end")
+            {
+                string[] input = inputString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                string homeTown = input[3];
+
+                if (IsStudentExisting(students, firstName, lastName))
+                {
+                    Student student = GetStudent(students, firstName, lastName);
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Age = age;
+                    student.HomeTown = homeTown;
+                }
+                else
+                {
+                    Student student = new Student(firstName, lastName, age, homeTown);
+
+                    students.Add(student);
+                }
+            }
+
+            string town = Console.ReadLine();
+
+            List<Student> filteredStudents = students
+                .Where(x => x.HomeTown == town)
+                .ToList();
+
+            foreach (Student student in filteredStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+
+        static Student GetStudent(List<Student> students, string firstName, string lastName)
+        {
+            Student existingStudent = null;
+
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    existingStudent = student;
+                }
+            }
+
+            return existingStudent;
+        }
+
+        static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+        {
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeTown = homeTown;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
