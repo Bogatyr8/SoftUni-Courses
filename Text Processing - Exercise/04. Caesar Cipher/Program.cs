7dd3bc5105bedbf99ab684ca9f633ee262b03a86
Boyan Boychev <boyanboychev@gmail.com>
2022-11-12 22:19:12 +0000

    Added task "04. Caesar Cipher" from Text Processing - Exercise

---
+++ Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Create a program that returns an encrypted version of the same text. Encrypt the text by shifting each character with three
+//positions forward. For example, A would be replaced by D, B would become E and so on. Print the encrypted text.
+            string input = Console.ReadLine();
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                output.Append((char)(input[i] + 3));
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
