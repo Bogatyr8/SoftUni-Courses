1e572e0a573f9a82bb8543a07255513fd7d6b067
Boyan Boychev <boyanboychev@gmail.com>
2022-11-13 01:24:48 +0000

    Added task "05. Multiply Big Number" from Text Processing - Exercise (Added ability to process negative number) (+ 1 bug still there)

---
+++ Text Processing - Exercise/05. Multiply Big Number/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//You are given two lines – the first one can be a really big number (0 to 1050). The second one will be a single-digit number (0 to 9). Your task is to display the product of these numbers.
+//Note: do not use the BigInteger class.
+            string input1 = Console.ReadLine();
+            int input2 = int.Parse(Console.ReadLine());
+            int credit = 0;
+            string input = input1;
+            if (input1[0] == '-')
+            {
+                input = input1.Substring(1);
+            }
+
+            StringBuilder product = new StringBuilder();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                int tempValue = ((input[i] - 48) * input2) + credit;
+                credit = tempValue / 10;
+                char currentDigit = (char)((tempValue % 10) + 48);
+                product.Append(currentDigit);
+            }
+            if (credit > 0)
+            {
+                product.Append(credit);
+            }
+            if (input1[0] == '-')
+            {
+                product.Append('-');
+            }
+            string strProduct = product.ToString();
+            char[] charArray = strProduct.ToCharArray();
+            Array.Reverse(charArray);
+            string output = new string(charArray);
+
+            Console.WriteLine(output);
+
+        }
+    }
+}
