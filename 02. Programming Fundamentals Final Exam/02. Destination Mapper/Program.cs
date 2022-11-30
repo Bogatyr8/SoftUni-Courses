4eb7add91e9dec3dd45b864d7420e492b154b9a0
Boyan Boychev <boyanboychev@gmail.com>
2022-11-30 08:08:32 +0000

    Added task "02. Destination Mapper" from 02. Programming Fundamentals Final Exam

---
+++ 02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Now that you have planned out your tour, you are ready to go!Your next task is to mark all the points on the map that you are going to visit.
+//You will be given a string representing some places on the map.You have to filter only the valid ones. A valid location is:
+//•	Surrounded by "=" or "/" on both sides(the first and the last symbols must match)
+//•	After the first "=" or "/" there should be only letters(the first must be upper -case, other letters could be upper or lower -case)
+//•	The letters must be at least 3
+//Example: In the string "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=" only the first two locations are valid.
+//After you have matched all the valid locations, you have to calculate travel points.They are calculated by summing the lengths of all the valid destinations that you have found on the map.
+//In the end, on the first line, print: "Destinations: {destinations joined by ', '}".
+//On the second line, print "Travel Points: {travel_points}".
+//Input / Constraints
+//•	You will receive a string representing the locations on the map
+//•	JavaScript: you will receive a single parameter: string
+//Output
+//•	Print the messages described above
+            string pattern = @"(\=|\/)(?<destination>[A-Z][A-Za-z]{2,})\1";
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            List<string> destinations = new List<string>();
+            int travelPoints = 0;
+            foreach (Match place in matches)
+            {
+                string destination = place.Groups["destination"].Value;
+                destinations.Add(destination);
+                travelPoints += destination.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
