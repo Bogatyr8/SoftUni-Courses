121f87f94be30c1ef535e209fc0448f8120bac50
Boyan Boychev <boyanboychev@gmail.com>
2022-12-03 22:11:25 +0000

    Added task "02. Emoji Detector" from 05. Programming Fundamentals Final Exam

--- 05. Programming Fundamentals Final Exam/01. Activation Keys/Program.cs
+++ 05. Programming Fundamentals Final Exam/01. Activation Keys/Program.cs
@@ -7,31 +7,31 @@ namespace _01._Activation_Keys
     {
         static void Main(string[] args)
         {
-//You are about to make some good money, but first, you need to think of a way to verify who paid for your product and who didn't. You
-//have decided to let people use the software for a free trial period and then require an activation key to continue using the product.
-//Before you can cash out, the last step is to design a program that creates unique activation keys for each user. So, waste no more
-//time and start typing!
-//The first line of the input will be your raw activation key.It will consist of letters and numbers only.
-//After that, until the "Generate" command is given, you will be receiving strings with instructions for different operations that
-//need to be performed upon the raw activation key.
-//There are several types of instructions, split by ">>>":
-//•	"Contains>>>{substring}":
-//o If the raw activation key contains the given substring, print         
-// "{raw activation key} contains {substring}".
-//o Otherwise, print "Substring not found!"
-//•	"Flip>>>Upper/Lower>>>{startIndex}>>>{endIndex}":
-//o Changes the substring between the given indices(the end index is exclusive) to upper or lower case and then prints the
-//activation key.
-//o All given indexes will be valid.
-//•	"Slice>>>{startIndex}>>>{endIndex}":
-//o Deletes the characters between the start and end indices(the end index is exclusive) and prints the activation key.
-//o Both indices will be valid.
-//Input
-//•	The first line of the input will be a string consisting of letters and numbers only.
-//•	After the first line, until the "Generate" command is given, you will be receiving strings.
-//Output
-//•	After the "Generate" command is received, print:
-//o "Your activation key is: {activation key}"
+            //You are about to make some good money, but first, you need to think of a way to verify who paid for your product and who didn't. You
+            //have decided to let people use the software for a free trial period and then require an activation key to continue using the product.
+            //Before you can cash out, the last step is to design a program that creates unique activation keys for each user. So, waste no more
+            //time and start typing!
+            //The first line of the input will be your raw activation key.It will consist of letters and numbers only.
+            //After that, until the "Generate" command is given, you will be receiving strings with instructions for different operations that
+            //need to be performed upon the raw activation key.
+            //There are several types of instructions, split by ">>>":
+            //•	"Contains>>>{substring}":
+            //o If the raw activation key contains the given substring, print         
+            // "{raw activation key} contains {substring}".
+            //o Otherwise, print "Substring not found!"
+            //•	"Flip>>>Upper/Lower>>>{startIndex}>>>{endIndex}":
+            //o Changes the substring between the given indices(the end index is exclusive) to upper or lower case and then prints the
+            //activation key.
+            //o All given indexes will be valid.
+            //•	"Slice>>>{startIndex}>>>{endIndex}":
+            //o Deletes the characters between the start and end indices(the end index is exclusive) and prints the activation key.
+            //o Both indices will be valid.
+            //Input
+            //•	The first line of the input will be a string consisting of letters and numbers only.
+            //•	After the first line, until the "Generate" command is given, you will be receiving strings.
+            //Output
+            //•	After the "Generate" command is received, print:
+            //o "Your activation key is: {activation key}"
             string activationKey = Console.ReadLine();
             string input;
             while ((input = Console.ReadLine()) != "Generate")
@@ -69,13 +69,16 @@ namespace _01._Activation_Keys
                         substring = substring.ToLower();
                         activationKey = activationKey.Remove(startIndex, wordLength);
                         activationKey = activationKey.Insert(startIndex, substring);
+                    }
+                    Console.WriteLine(activationKey);
                 }
                 else if (command == "Slice")
                 {
-                    startIndex = int.Parse(order[1]);
-                    endIndex = int.Parse(order[2]);
-                    wordLength = endIndex - startIndex;
+                    int startIndex = int.Parse(order[1]);
+                    int endIndex = int.Parse(order[2]);
+                    int wordLength = endIndex - startIndex;
                     activationKey = activationKey.Remove(startIndex, wordLength);
+                    Console.WriteLine(activationKey);
                 }
             }
 
---
+++ 05. Programming Fundamentals Final Exam/02. Emoji Detector/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _02._Emoji_Detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Your task is to write a program that extracts emojis from a text and finds the threshold based on the input.
+//You have to get your Cool threshold.It is obtained by multiplying all the digits found in the input. The cool threshold could be a
+//huge number, so be mindful.
+//An emoji is valid when:
+//•	It is surrounded by 2 characters, either "::" or "**".
+//•	It is at least 3 characters long(without the surrounding symbols).
+//•	It starts with a capital letter.
+//•	Continues with lowercase letters only.
+//Examples of valid emojis: ::Joy::, **Banana**, ::Wink::
+//Examples of invalid emojis: ::Joy**, ::fox:es:, **Monk3ys**, :Snak::Es::
+//You need to count all valid emojis in the text and calculate their coolness. The coolness of the emoji is determined by summing
+//all the ASCII values of all letters in the emoji. 
+//Examples: ::Joy:: - 306, **Banana** -577, ::Wink:: - 409
+//You need to print the result of the cool threshold and after that – to take all emojis out of the text, count them and print only
+//the cool ones on the console.
+//Input
+//•	On the single input, you will receive a piece of string.
+//Output
+//•	On the first line of the output, print the obtained Cool threshold in the format:
+//"Cool threshold: {coolThresholdSum}"
+//•	On the following line, print the count of all emojis found in the text in format:
+//"{countOfAllEmojis} emojis found in the text. The cool ones are:
+//{ cool emoji 1}
+//{ cool emoji 2}
+//…
+//{ cool emoji N}"
+//Constraints
+//There will always be at least one digit in the text!
+            string pattern = @"(?<digits>\d)|(?<emojies>(\:\:|\*\*)[A-Z][a-z]{2,}\1)";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+            MatchCollection matches = regex.Matches(input);
+            List<int> digits = new List<int>();
+            List<string> emojies = new List<string>();
+            foreach (Match item in matches)
+            {
+                if (int.TryParse(item.Groups["digits"].Value, out int value))
+                {
+                    digits.Add(value);
+                }
+                else
+                {
+                    emojies.Add(item.Groups["emojies"].Value);
+                }
+            }
+            BigInteger coolTreshold = 1;
+            foreach (int digit in digits)
+            {
+                coolTreshold *= digit;
+            }
+            int mathesFound = emojies.Count;
+            emojies = emojies
+                .Where(e => SumOfChars(e) >= coolTreshold)
+                .ToList();
+            Console.WriteLine($"Cool threshold: {coolTreshold}");
+            Console.WriteLine($"{mathesFound} emojis found in the text. \nThe cool ones are:");
+            foreach (string emoji in emojies)
+            {
+                Console.WriteLine(emoji);
+            }
+        }
+
+        private static ulong SumOfChars(string input)
+        {
+            char[] separator = new char[] { ':', '*' };
+            string[] trimmer = input.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            input = trimmer[0];
+            ulong sumChars = 0;
+            foreach (char ch in input)
+            {
+                sumChars += (ulong)ch;
+            }
+            return sumChars;
+        }
+    }
+}
