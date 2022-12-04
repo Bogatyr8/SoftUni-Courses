f4cb65f9669c59757c8c5ad404451d7f15f9e6e8
Boyan Boychev <boyanboychev@gmail.com>
2022-12-04 16:34:07 +0000

    Added task "02. Easter Eggs" from Programming Fundamentals Final Exam - 4 December 2022

---
+++ Programming Fundamentals Final Exam - 4 December 2022/02. Easter Eggs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Easter_Eggs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"((\@|\#)+)(?<color>[a-z]{3,})((\@|\#)+)([^A-Za-z0-9]*)((\/)+)(?<amount>\d+)((\/)+)";
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match egg in matches)
+            {
+                string eggColor = egg.Groups["color"].Value;
+                string eggAmount = egg.Groups["amount"].Value;
+
+                Console.WriteLine($"You found {eggAmount} {eggColor} eggs!");
+            }
+        }
+    }
+}
