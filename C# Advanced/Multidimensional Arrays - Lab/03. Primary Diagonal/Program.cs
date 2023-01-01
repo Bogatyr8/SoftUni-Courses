64bfe76d35af098d67df93835bb7fdb41dade443
Boyan Boychev <boyanboychev@gmail.com>
2023-01-01 21:10:22 +0000

    Added task "04. Symbol in Matrix" from Multidimensional Arrays - Lab

--- C# Advanced/Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
+++ C# Advanced/Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -6,10 +6,10 @@
     {
         static void Main(string[] args)
         {
-//Create a program that finds the sum of elements from the matrix’s primary diagonal.
-//Input
-//•	On the first line, you are given the integer N – the size of the square matrix.
-//•	The next N lines, hold the values for every row – N numbers separated by a space.
+            //Create a program that finds the sum of elements from the matrix’s primary diagonal.
+            //Input
+            //•	On the first line, you are given the integer N – the size of the square matrix.
+            //•	The next N lines, hold the values for every row – N numbers separated by a space.
             int n = int.Parse(Console.ReadLine());
             int[,] sqMatrix = new int[n, n];
             for (int row = 0; row < n; row++)
---
+++ C# Advanced/Multidimensional Arrays - Lab/04. Symbol in Matrix/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _04._Symbol_in_Matrix
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Create a program that reads N, a number representing rows and cols of a matrix. On the next N lines, you
+//will receive rows of the matrix.Each row consists of ASCII characters. After that, you will receive a
+//symbol. Find the first occurrence of that symbol in the matrix and print its position in the format:
+//"({row}, {col})".If there is no such symbol, print an error message "{symbol} does not occur in the
+//matrix".
+            int n = int.Parse(Console.ReadLine());
+            char[,] sqMatrix = new char[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                char[] colElem = Console.ReadLine()
+                    .ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    sqMatrix[row, col] = colElem[col];
+                }
+            }
+
+            char seekChar = char.Parse(Console.ReadLine());
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (sqMatrix[row, col] == seekChar)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{seekChar} does not occur in the matrix");
+        }
+    }
+}
