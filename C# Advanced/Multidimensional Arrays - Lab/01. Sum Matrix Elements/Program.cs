b8fae435e6d36373942c835f268e401371b2be58
Boyan Boychev <boyanboychev@gmail.com>
2023-01-01 20:08:10 +0000

    Added task "01. Sum Matrix Elements" from Multidimensional Arrays - Lab

---
+++ C# Advanced/Multidimensional Arrays - Lab/01. Sum Matrix Elements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Write a program that reads a matrix from the console and prints:
+//•	Count of rows
+//•	Count of columns
+//•	Sum of all matrix elements
+//On the first line, you will get matrix sizes in format[rows, columns]
+            int[] dimensions = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+            int sum = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
