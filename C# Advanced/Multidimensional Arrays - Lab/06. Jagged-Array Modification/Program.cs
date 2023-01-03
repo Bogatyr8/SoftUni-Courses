e8d216acc0519699aa37dc64ee49a931217f1891
Boyan Boychev <boyanboychev@gmail.com>
2023-01-03 20:26:53 +0000

    Added task "07. Pascal Triangle" from Multidimensional Arrays - Lab

--- C# Advanced/Multidimensional Arrays - Lab/06. Jagged-Array Modification/Program.cs
+++ C# Advanced/Multidimensional Arrays - Lab/06. Jagged-Array Modification/Program.cs
@@ -6,13 +6,13 @@
     {
         static void Main(string[] args)
         {
-//Create a program that reads a matrix from the console. On the first line, you will get matrix rows. On
-//the next rows lines, you will get elements for each column separated with space.You will be receiving
-//commands in the following format:
-//•	Add { row} { col} { value} – Increase the number at the given coordinates with the value.
-//•	Subtract { row} { col} { value} – Decrease the number at the given coordinates by the value.
-//Coordinates might be invalid. In this case, you should print "Invalid coordinates".When you receive
-//"END" you should print the matrix and stop the program.
+            //Create a program that reads a matrix from the console. On the first line, you will get matrix rows. On
+            //the next rows lines, you will get elements for each column separated with space.You will be receiving
+            //commands in the following format:
+            //•	Add { row} { col} { value} – Increase the number at the given coordinates with the value.
+            //•	Subtract { row} { col} { value} – Decrease the number at the given coordinates by the value.
+            //Coordinates might be invalid. In this case, you should print "Invalid coordinates".When you receive
+            //"END" you should print the matrix and stop the program.
             int n = int.Parse(Console.ReadLine());
             int[][] jagged = new int[n][];
             for (int row = 0; row < jagged.Length; row++)
---
+++ C# Advanced/Multidimensional Arrays - Lab/07. Pascal Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _07._Pascal_Triangle
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//The Pascal’s triangle may be constructed in the following manner: in row 0(the topmost row), there is a
+//unique nonzero entry 1.Each entry of each subsequent row is constructed by adding the number above and
+//to the left with the number above and to the right, treating blank entries as 0.
+//Write a program to print the Pascal’s triangle of given size n.
+            long n = long.Parse(Console.ReadLine());
+            long[][] pascal = new long[n][];
+            long column = 1;
+            pascal[0] = new long[1];
+            pascal[0][0] = 1;
+            for (int row = 1; row < pascal.Length; row++)
+            {
+                pascal[row] = new long[row + 1];
+                pascal[row][0] = 1;
+                for (int col = 1; col < row; col++)
+                {
+                    pascal[row][col] = pascal[row - 1][col - 1] + pascal[row - 1][col];
+                }
+                pascal[row][pascal[row].Length - 1] = 1;
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                Console.WriteLine(String.Join(" ", pascal[row]));
+            }
+        }
+    }
+}
