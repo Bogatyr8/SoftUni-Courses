3b46a3f971e178f7355f65711fdccdb308eca077
Boyan Boychev <boyanboychev@gmail.com>
2023-01-13 01:42:09 +0000

    Added task "02. Average Student Grades" from Sets and Dictionaries Advanced - Lab

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02._Average_Student_Grades
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Create a program, which reads a name of a student and his / her grades and adds them to the
+//student record, then prints the students' names with their grades and their average grade.
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                decimal value = decimal.Parse(input[1]);
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(input[0], new List<decimal>());
+                }
+                students[name].Add(value);
+            }
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Key} -> {String.Join(" ", item.Value)} (avg: {item.Value.Average():f2})");
+            }
+        }
+    }
+}
