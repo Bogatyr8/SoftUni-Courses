28926497214d29943839042639237adc399eba15
Boyan Boychev <boyanboychev@gmail.com>
2023-01-14 01:29:16 +0000

    Added task "06. Record Unique Names" from Sets and Dictionaries Advanced - Lab

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06._Record_Unique_Names
+{
+    using System;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ //Create a program, which will take a list of names and print only the unique names in the list.
+            HashSet<string> names = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            Console.WriteLine("===================");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
