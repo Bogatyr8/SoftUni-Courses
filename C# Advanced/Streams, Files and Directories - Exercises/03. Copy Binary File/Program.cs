f04e26ada6fd06cdab252d72bf8fcfcaaa423eb0
Boyan Boychev <boyanboychev@gmail.com>
2023-01-22 22:02:04 +0000

    Added task "03. Copy Binary File" from Streams, Files and Directories - Exercises

---
+++ C# Advanced/Streams, Files and Directories - Exercises/03. Copy Binary File/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+//Write a program that copies the contents of a binary file(e.g.copyMe.png) to another
+//binary file(e.g.copyMe-copy.png) using FileStream.You are not allowed to use the File
+//class or similar helper classes.
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream source = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (FileStream output = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[512];
+                        int size = source.Read(buffer, 0, 512);
+                        if (size == 0)
+                        {
+                            break;
+                        }
+                        output.Write(buffer);
+                    }
+                }
+            }
+        }
+    }
+}
