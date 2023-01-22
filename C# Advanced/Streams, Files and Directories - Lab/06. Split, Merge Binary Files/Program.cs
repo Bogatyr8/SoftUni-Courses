670c2210dd425c54a15f9c4f0d713e7e2ff624f9
Boyan Boychev <boyanboychev@gmail.com>
2023-01-22 19:01:56 +0000

    Added task "07. Folder Size" from Streams, Files and Directories - Lab

--- C# Advanced/Streams, Files and Directories - Lab/06. Split, Merge Binary Files/Program.cs
+++ C# Advanced/Streams, Files and Directories - Lab/06. Split, Merge Binary Files/Program.cs
@@ -6,6 +6,12 @@
     {
         static void Main()
         {
+//You are given an input binary file(e.g.example.png).Write a program to split it into two
+//equal-sized files(e.g.part - 1.bin and part - 2.bin).When the input file size is an odd number,
+//the first part should be 1 byte bigger than the second.
+//After splitting the input file, join the obtained files into a new file(e.g.example -
+//joined.png).The obtained result file should be the same as the initial input file.
+
             string sourceFilePath = @"..\..\..\Files\example.png";
             string joinedFilePath = @"..\..\..\Files\example-joined.png";
             string partOnePath = @"..\..\..\Files\part-1.bin";
