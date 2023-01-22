9fc66203d3e404c93794148d4e05f78ebc3a7706
Boyan Boychev <boyanboychev@gmail.com>
2023-01-22 21:29:32 +0000

    Added task "02. Line Numbers" from Streams, Files and Directories - Exercises

---
+++ C# Advanced/Streams, Files and Directories - Exercises/02. Line Numbers/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+//Write a program that reads a text file(e.g.text.txt) and inserts line numbers in front
+//of each of its lines and count all the letters and punctuation marks. The result should
+//be written to another text file(e.g.output.txt).Use the static class File to read and
+//write all the lines of the input and output files.
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int leters = 0;
+            int otherCharacters = 0;
+            int countLine = 0;
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                leters = 0;
+                otherCharacters = 0;
+                foreach (string word in words)
+                {
+                    foreach (char ch in word)
+                    {
+                        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+                        {
+                            leters++;
+                        }
+                        else
+                        {
+                            otherCharacters++;
+                        }
+                    }
+                }
+
+                lines[countLine] = $"Line {countLine} {lines[countLine]} ({leters})({otherCharacters})";
+                countLine++;
+            }
+
+            File.WriteAllLines(outputFilePath, lines);
+        }
+    }
+}
