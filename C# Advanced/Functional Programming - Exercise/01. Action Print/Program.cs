0c6a29d3630cd6582de07fbd2ab8f85c7217e8be
Boyan Boychev <boyanboychev@gmail.com>
2023-01-30 22:36:14 +0000

    Added task "01. Action Print" from Functional Programming - Exercise

---
+++ C# Advanced/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,9 @@
+﻿//Create a program that reads a collection of strings from the console and then prints them onto
+//the console. Each name should be printed on a new line. Use Action<T>.
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Action<string[]> PrintName = (names)
+    => Console.WriteLine(String.Join(Environment.NewLine, names));
+
+PrintName(names);
