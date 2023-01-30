42f3f3895fc4f1e20ab76b1fd1ace1fbeeba3098
Boyan Boychev <boyanboychev@gmail.com>
2023-01-30 23:35:00 +0000

    Added task "03. Custom Min Function" from Functional Programming - Exercise

---
+++ C# Advanced/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,11 @@
+﻿//Create a simple program that reads from the console a set of integers and prints back on the
+//console the smallest number from the collection. Use Func<T, T>.
+
+string input = Console.ReadLine();
+Func<string, int> parser = n => int.Parse(n);
+int[] numbers = input
+    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(parser)
+    .ToArray();
+
+Console.WriteLine(numbers.Min());
