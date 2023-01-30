9cfefa5ed5f0d8f16fa40503c0aa94f0bada924e
Boyan Boychev <boyanboychev@gmail.com>
2023-01-30 23:17:32 +0000

    Added task "02. Knights of Honor" from Functional Programming - Exercise

---
+++ C# Advanced/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,11 @@
+﻿//Create a program that reads a collection of names as strings from the console, appends "Sir" in
+//front of every name and prints it back in the console. Use Action<T>.
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Action<string> print = msg => Console.WriteLine("Sir "+ msg);
+foreach (string name in names)
+{
+    print(name);
+}
