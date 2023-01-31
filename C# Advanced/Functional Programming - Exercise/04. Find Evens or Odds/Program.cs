e6ee9d4ee5cfce8d0fab86c4574b7be87e8937e3
Boyan Boychev <boyanboychev@gmail.com>
2023-01-31 00:31:40 +0000

    Added task "04. Find Evens or Odds" from Functional Programming - Exercise

---
+++ C# Advanced/Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,48 @@
+﻿//You are given a lower and an upper bound for a range of integer numbers. Then a command
+//specifies if you need to list all even or odd numbers in the given range. Use Predicate<T>.
+
+int[] margin = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(n => int.Parse(n))
+    .ToArray();
+int lower = margin[0];
+int upper = margin[1];
+
+string filter = Console.ReadLine();
+Predicate<int> odd = n => n % 2 == 1;
+Predicate<int> even = n => n % 2 == 0;
+
+Func<int, int, List<int>> generateList =
+ (lower, upper) => {
+     List<int> range = new();
+     for (int i = Math.Min(lower, upper); i <= Math.Max(lower, upper); i++)
+{
+    range.Add(i);
+}
+return range;
+};
+
+List<int> numbers = generateList(lower, upper);
+
+Predicate<int> match;
+
+if (filter == "even")
+{
+    match = number => number % 2 == 0;
+}
+else
+{
+    match = number => number % 2 != 0;
+}
+
+
+List<int> result = new();
+
+foreach (var number in numbers)
+{
+    if (match(number))
+    {
+        result.Add(number);
+    }
+}
+    Console.WriteLine(string.Join(" ", result));
