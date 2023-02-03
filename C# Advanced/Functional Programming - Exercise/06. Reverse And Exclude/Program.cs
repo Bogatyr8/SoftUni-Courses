6c5016ce2d63f02e46744264b3ca939d9b8461a7
Boyan Boychev <boyanboychev@gmail.com>
2023-02-03 01:33:18 +0000

    Added task "06. Reverse And Exclude" from Functional Programming - Exercise

---
+++ C# Advanced/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,40 @@
+﻿//Create a program that reverses a collection and removes elements that are divisible by a given
+//integer n. Use predicates/functions.
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+Func<List<int>, List<int>> reversed = input =>
+{
+    List<int> output = new();
+    for (int i = input.Count - 1; i >= 0; i--)
+    {
+        output.Add(input[i]);
+    }
+    return output;
+};
+Func<List<int>, Predicate<int>, List<int>> transform = (input, match) =>
+{
+    List<int> output = new();
+    foreach (var item in input)
+    {
+        if (!match(item))
+        {
+            output.Add(item);
+        }
+    }
+    return output;
+};
+
+List<int> input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+int div = int.Parse(Console.ReadLine());
+
+input = transform(input, x => x % div == 0);
+input = reversed(input);
+
+Console.WriteLine(String.Join(" ", input));
+
