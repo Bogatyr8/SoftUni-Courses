54f45cb10956b2e0cd736057b412a84543e89104
Boyan Boychev <boyanboychev@gmail.com>
2023-02-12 15:19:24 +0000

    Added task "04. Opinion Poll" from Defining Classes - Exercise

---
+++ C# Advanced/Defining Classes - Exercise/04. Opinion Poll/Family.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses;
+public class Family
+{
+    private List<Person> people;
+    public Family()
+    {
+        this.people = new();
+    }
+
+    public void AddMember(Person person)
+    {
+        if (person.Age <= 30)
+        {
+            return;
+        }
+        this.people.Add(person);
+    }
+    public void PrintMembers()
+    {
+        foreach (var member in people.OrderBy(p => p.Name).ToList())
+        {
+            Console.WriteLine($"{member.Name} - {member.Age}");
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,29 @@
+﻿//Using the Person class, write a program that reads from the console N lines of personal
+//information and then prints all people, whose age is more than 30 years, sorted in
+//alphabetical order.
+
+using System;
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        Family family = new();
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = input[0];
+            int age = int.Parse(input[1]);
+
+            Person person = new(name, age);
+            family.AddMember(person);
+        }
+
+        family.PrintMembers();
+    }
+}
