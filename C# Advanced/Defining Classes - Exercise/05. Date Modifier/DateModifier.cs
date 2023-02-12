8a7c8b77ab18d87619993c94478fc13b132e36be
Boyan Boychev <boyanboychev@gmail.com>
2023-02-12 16:32:16 +0000

    Added task "05. Date Modifier" from Defining Classes - Exercise

---
+++ C# Advanced/Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Modifier;
+
+public static class DateModifier
+{
+    public static int DateDifference(string start, string end)
+    {
+        DateTime startDate = DateTime.Parse(start);
+        DateTime endDate = DateTime.Parse(end);
+
+        TimeSpan difference = startDate - endDate;
+        return Math.Abs(difference.Days);
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,18 @@
+﻿//Create a class DateModifier, which stores the difference of the days between two dates.
+//It should have a method that takes two string parameters, representing dates as strings and
+//calculates the difference in the days between them. 
+using System;
+namespace Date_Modifier;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        string start = Console.ReadLine();
+        string end = Console.ReadLine();
+
+        int differenceInDays = DateModifier.DateDifference(start, end);
+
+        Console.WriteLine(differenceInDays);
+    }
+}
