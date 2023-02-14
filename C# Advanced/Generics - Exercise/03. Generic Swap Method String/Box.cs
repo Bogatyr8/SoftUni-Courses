562947f1d9823ffc6baa9e9fd3746d4168f89be5
Boyan Boychev <boyanboychev@gmail.com>
2023-02-14 19:00:45 +0000

    Added task "03. Generic Swap Method String" from Generics - Exercise

---
+++ C# Advanced/Generics - Exercise/03. Generic Swap Method String/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethodSwapStrings;
+
+public class Box<T>
+{
+    private List<T> list;
+
+    public Box() 
+    {
+        list = new List<T>();
+    }
+    public void Add(T value)
+    {
+        list.Add(value);
+    }
+    public void Swap(int firstIndex, int secondIndex)
+    {
+        T temp = list[firstIndex];
+        list[firstIndex] = list[secondIndex];
+        list[secondIndex] = temp;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder(); 
+        foreach (T item in list)
+        {
+            sb.AppendLine($"{typeof(T)}: {item}");
+        }
+        return sb.ToString().TrimEnd();
+    }
+}
---
+++ C# Advanced/Generics - Exercise/03. Generic Swap Method String/Program.cs
@@ -0,0 +1,26 @@
+﻿//Create a generic method that receives a list, containing any type of data and swaps the elements
+//at two given indexes.
+//Input
+//On the first line, you will read n number of boxes of type string and add them to the list.
+//On the next line, however, you will receive a swap command consisting of two indexes.
+//Output
+//Use the method you've created to swap the elements that correspond to the given indexes and print
+//each element in the list.
+
+using GenericMethodSwapStrings;
+
+Box<string> box = new();
+
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+    box.Add(input);
+}
+int[] indexInput = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+box.Swap(indexInput[0], indexInput[1]);
+
+Console.WriteLine(box.ToString());
