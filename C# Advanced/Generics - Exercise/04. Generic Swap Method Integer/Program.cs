184a8a4f274ad8836d4bc647e63979b09ac137a5
Boyan Boychev <boyanboychev@gmail.com>
2023-02-14 19:05:24 +0000

    Added task "04. Generic Swap Method Integer" from Generics - Exercise

---
+++ C# Advanced/Generics - Exercise/04. Generic Swap Method Integer/Program.cs
@@ -0,0 +1,20 @@
+﻿//Use the description of the previous problem, but now, test your list of generic boxes
+//with integers.
+
+using GenericMethodSwapIntegers;
+
+Box<int> box = new();
+
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    box.Add(input);
+}
+int[] indexInput = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+box.Swap(indexInput[0], indexInput[1]);
+
+Console.WriteLine(box.ToString());
