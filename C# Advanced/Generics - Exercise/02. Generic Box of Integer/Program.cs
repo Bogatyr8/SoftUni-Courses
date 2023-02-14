03a1781076edc8e0fd1c84d12c8fae5ba1101bcf
Boyan Boychev <boyanboychev@gmail.com>
2023-02-14 18:34:15 +0000

    Added task "02. Generic Box of Integer" from Generics - Exercise

---
+++ C# Advanced/Generics - Exercise/02. Generic Box of Integer/Program.cs
@@ -0,0 +1,13 @@
+﻿//Use the description of the previous problem but now, test your generic box with Integers.
+using BoxOfInt;
+
+Box<int> box = new();
+
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    box.Add(input);
+}
+
+Console.WriteLine(box.ToString());
