88a618e9de1e371c1e2887c4400968c81c8b6b9e
Boyan Boychev <boyanboychev@gmail.com>
2023-02-25 23:19:32 +0000

    Added task "04. Random List" to Inheritance - Lab

---
+++ C# OOP/Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+        public RandomList()
+        {
+            random = new Random();  
+        }
+        public string RandomString() 
+        { 
+            int index = random.Next(0, this.Count);
+            string removedElement = this[index];
+            this.RemoveAt(index);
+            return removedElement;
+        }
+    }
+}
---
+++ C# OOP/Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,28 @@
+﻿//NOTE: You need a public StartUp class with the namespace CustomRandomList.
+//Create a RandomList class that has all the functionality of List<string>.
+//Add an additional function that returns and removes a random element from
+//the list.
+//Public method: RandomString(): string
+using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        
+        RandomList list = new RandomList();
+        list.Add("Boyan");
+        list.Add("Chara");
+        list.Add("Zori");
+        list.Add("Penka");
+        list.Add("Marti");
+        list.Add("Mitko");
+        list.Add("Cveti");
+        list.Add("Vanya");
+        list.Add("Stefka");
+        Console.WriteLine(list.RandomString());
+    }
+}
