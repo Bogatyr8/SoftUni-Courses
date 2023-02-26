0c31316993221d6a927ae5dff5f788818a5a0ad9
Boyan Boychev <boyanboychev@gmail.com>
2023-02-26 01:11:53 +0000

    Added task "05. Stack of Strings" to Inheritance - Lab

---
+++ C# OOP/Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStacks
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+            foreach (var item in range)
+            {
+                Push(item);
+            }
+            return this;
+        }
+    }
+}
---
+++ C# OOP/Inheritance - Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,27 @@
+﻿//NOTE: You need a public StartUp class with the namespace CustomStack.
+//Create a class StackOfStrings that extends Stack, can store only strings,
+//and has the following functionality:
+//Public method: IsEmpty(): bool
+//Public method: AddRange(): Stack<string>
+
+using CustomStacks;
+using System;
+using System.Collections.Generic;
+
+namespace CustomStack;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        StackOfStrings stackOfStrings = new StackOfStrings();
+
+        stackOfStrings.AddRange(new List<string>() { "1", "2", "3", "4", });
+        stackOfStrings.AddRange(new List<string>() { "5", "6", "7", "8", });
+
+        while (!stackOfStrings.IsEmpty())
+        {
+            Console.WriteLine(stackOfStrings.Pop());
+        }
+    }
+}
