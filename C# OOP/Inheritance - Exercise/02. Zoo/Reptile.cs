73251d6421bb8cec17d198156dd902a8cc630742
Boyan Boychev <boyanboychev@gmail.com>
2023-02-26 13:24:06 +0000

    Added task "02. Zoo" to Inheritance - Exercise

---
+++ C# OOP/Inheritance - Exercise/02. Zoo/Reptile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public class Reptile : Animal
+    {
+        public Reptile(string name) : base(name)
+        {
+
+        }
+    }
+}
