d34fdbf557601262b0a18cba64c608447adc9d13
Boyan Boychev <boyanboychev@gmail.com>
2023-02-26 13:42:48 +0000

    Added task "03. Players and Monsters" to Inheritance - Exercise

---
+++ C# OOP/Inheritance - Exercise/03. Players and Monsters/Elf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class Elf : Hero
+    {
+        public Elf(string username, int level) : base(username, level)
+        {
+
+        }
+    }
+}
---
+++ C# OOP/Inheritance - Exercise/03. Players and Monsters/SoulMaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class SoulMaster : DarkWizard
+    {
+        public SoulMaster(string username, int level) : base(username, level)
+        {
+
+        }
+    }
+}
