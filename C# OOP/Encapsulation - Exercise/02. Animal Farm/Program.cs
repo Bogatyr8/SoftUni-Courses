65dad1ebee50b52389cf2d4dce810408ab225775
Boyan Boychev <boyanboychev@gmail.com>
2023-03-02 23:41:49 +0000

    Added task "01. Class Box Data" from Encapsulation - Exercise

---
+++ C# OOP/Encapsulation - Exercise/02. Animal Farm/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(chicken.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
