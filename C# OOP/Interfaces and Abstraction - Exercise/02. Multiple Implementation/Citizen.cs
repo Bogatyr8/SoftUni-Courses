40b98095e16634736567005f38811daa75e0ca9d
Boyan Boychev <boyanboychev@gmail.com>
2023-03-06 23:06:56 +0000

    Added task "02. Multiple Implementation" from Interfaces and Abstraction - Exercise

---
+++ C# OOP/Interfaces and Abstraction - Exercise/02. Multiple Implementation/Citizen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo;
+
+public class Citizen : IPerson, IIdentifiable, IBirthable
+{
+    public Citizen(string name, int age, string id, string birthdate)
+    {
+        Name = name;
+        Age = age;
+        Id = id;
+        Birthdate = birthdate;
+    }
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Id { get; set; }
+    public string Birthdate { get; set; }
+}
