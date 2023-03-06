c7ab82d5e5383788b8db49588efdde2f16d4a591
Boyan Boychev <boyanboychev@gmail.com>
2023-03-06 22:37:19 +0000

    Added task "01. Define an Interface IPerson" from Interfaces and Abstraction - Exercise

---
+++ C# OOP/Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo;
+
+public class Citizen : IPerson
+{
+    public Citizen(string name, int age)
+    {
+        Age = age;
+        Name = name;
+    }
+
+    public int Age { get; set; }
+    public string Name { get; set; }
+}
---
+++ C# OOP/Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/StartUp.cs
@@ -0,0 +1,18 @@
+﻿//NOTE: You need a public StartUp class with the namespace PersonInfo.
+//Define an interface IPerson with properties for Name and Age. Define a class
+//Citizen that implements IPerson and has two properties string name and an int age.
+//The Citizen should accept name and age upon initialization.
+using System;
+
+namespace PersonInfo;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        IPerson person = new Citizen(name, age);
+        Console.WriteLine(person.Name);
+        Console.WriteLine(person.Age);
+    }
+}
